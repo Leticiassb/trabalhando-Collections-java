48d97ac117cbd5fdad69374c0b1936a646801f7d
Letícia Silva <leticiassb99@gmail.com>
2024-06-04 19:00:04 -0300

    Adicao de classes para exemplificar Set - Convidado, ConjuntoConvidados, AgendaContatos e Contato
    Signed-off-by: Letícia Silva <leticiassb99@gmail.com>

---
+++ src/main/java/list/operacoesBasicas/ConjuntoConvidados.java
@@ -0,0 +1,47 @@
+package main.java.list.operacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados() {
+        this.convidadoSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidadoSet.add(new Convidado(nome, codigoConvite));
+    }
+
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoParaRemover = null;
+        for(Convidado c : convidadoSet){
+            if(c.getCodigoConvite() == codigoConvite){
+                convidadoParaRemover = c;
+                break;
+            }
+        }
+        convidadoSet.remove(convidadoParaRemover);
+
+    }
+
+    public int contarConvidados(){
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println(convidadoSet);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+
+        conjuntoConvidados.adicionarConvidado("Joao", 1);
+        conjuntoConvidados.adicionarConvidado("Jaque", 2);
+        conjuntoConvidados.adicionarConvidado("Rose", 3);
+
+        System.out.println(conjuntoConvidados.contarConvidados());
+
+    }
+}
---
+++ src/main/java/list/pesquisa/AgendaContatos.java
@@ -0,0 +1,63 @@
+package main.java.list.pesquisa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero){
+        contatoSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContato(){
+        System.out.println(contatoSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome){
+        Set<Contato> contatosPorNome = new HashSet<>();
+
+        for(Contato c : contatoSet){
+            if (c.getNome().startsWith(nome)){
+                contatosPorNome.add(c);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero){
+        Contato contatoAtualizado = null;
+        for (Contato c : contatoSet){
+            if (c.getNome().equalsIgnoreCase(nome)) {
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.exibirContato();
+
+        agendaContatos.adicionarContato("Joao", 123);
+      //não adiciona nomes iguais
+        // agendaContatos.adicionarContato("Joao", 123);
+        agendaContatos.adicionarContato("Maria", 865);
+        agendaContatos.adicionarContato("Marcos", 965);
+        agendaContatos.adicionarContato("Rosa", 555);
+        agendaContatos.adicionarContato("Eduardo", 415);
+
+        agendaContatos.exibirContato();
+
+        System.out.println(agendaContatos.pesquisarPorNome("Marcos"));
+
+
+    }
+}
